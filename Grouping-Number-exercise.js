6b52eece5fd77150026e1bb5534bc7f5428e46d0
Panji SN <panji1595@gmail.com>
2019-08-05 18:35:20 +0700

    Adding exercise for friday

---
+++ Grouping-Number-exercise.js
@@ -0,0 +1,23 @@
+function mengelompokkanAngka(arr) {
+    var genap = [];
+    var ganjil = [];
+    var perkalianTiga = [];
+    for (var i = 0; i<arr.length;i++){
+        if (Number(arr[i]) % 3 == 0){
+            perkalianTiga = perkalianTiga + arr[i];
+        } else if (Number(arr[i]) % 2 == 0) {
+            genap = genap + arr[i];
+        } else {
+            ganjil = ganjil + arr[i];
+        }
+    } 
+    var total = [genap, ganjil, perkalianTiga];
+    return total;
+  }
+
+  
+  // TEST CASES
+  console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+  console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+  console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+  console.log(mengelompokkanAngka([])); // [ [], [], [] ]
